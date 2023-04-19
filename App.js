deee7eaeb52791e20dc87c990ccef79889efeb34
SINMANY <manysin48@gmail.com>
2023-04-19 22:08:11 +0700

    first commit-byMany

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import Products from './components/Porducts';
+
+function App() {
+    return (
+        <div>
+            <Products />
+        </div>
+
+    );
+}
+
+export default App;

27190ca2d152b4751e2debefcd5b3a0f053fc4eb
SINMANY <manysin48@gmail.com>
2023-04-26 16:42:11 +0700

    mony

--- components/ButtonCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function ButtonCard({ title, styleBtn }) {
-    const mainStyle = "btn btn-primary"
-    return (
-        <div className={styleBtn ? styleBtn : mainStyle}>{title ? title : "btn"}</div>
-    )
-}
